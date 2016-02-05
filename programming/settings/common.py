631ff4eae3e56be0a59dc3b0aee426e09e13eabf
sarkmen <beomjun.gim3@gmail.com>
2016-02-05 12:35:24 +0900

    prod

--- programming/settings/common.py
+++ programming/settings/common.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from os.path import abspath, dirname
+from glob import glob
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
